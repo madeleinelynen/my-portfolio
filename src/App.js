9b90547d3274fde20ab23e55d813fc9206a6d853
Maddy <Madeleine.lynen@gmx.net>
2025-07-18 20:14:37 +0200

    fixed case sensitive error

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import Home from './Home';
 import './App.css';
 import './style.css';
 
-import Flamecoach from './pages/Flamecoach';
-import Flamecoach2 from './pages/Flamecoach2';
-import Aufwind from './pages/Aufwind';
-import OddyVR from './pages/OddyVR';
-import VodafoneOMR from './pages/VodafoneOMR';
-import Vodafone5GCar from './pages/Vodafone5GCar';
-import SoftRevision from './pages/SoftRevision';
-import SchumannVR from './pages/SchumannVR';
-import Essen1887 from './pages/Essen1887';
-import ExperienceTirol from './pages/ExperienceTirol';
-import TerraInfrastructure from './pages/TerraInfrastructure';
-import Trikottaufe from './pages/Trikottaufe';
+import Flamecoach from './pages/Flamecoach.jsx';
+import Flamecoach2 from './pages/Flamecoach2.jsx';
+import Aufwind from './pages/Aufwind.jsx';
+import OddyVR from './pages/OddyVR.jsx';
+import VodafoneOMR from './pages/VodafoneOMR.jsx';
+import Vodafone5GCar from './pages/Vodafone5GCar.jsx';
+import SoftRevision from './pages/SoftRevision.jsx';
+import SchumannVR from './pages/SchumannVR.jsx';
+import Essen1887 from './pages/Essen1887.jsx';
+import ExperienceTirol from './pages/ExperienceTirol.jsx';
+import TerraInfrastructure from './pages/TerraInfrastructure.jsx';
+import Trikottaufe from './pages/Trikottaufe.jsx';
 
 function App() {
   return (
